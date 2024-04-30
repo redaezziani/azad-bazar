9e2d6d19daf383c3f098924e03f785fc08e877e3
rachidfrex <2003043000250@ofppt-edu.ma>
2024-04-30 18:24:19 +0100

    Update styling in footer component

--- src/components/home-page/footer.tsx
+++ src/components/home-page/footer.tsx
@@ -1,72 +1,86 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <footer
-    className='w-full bg-zinc-100  flex gap-5 justify-center flex-col items-center  py-4 px-4 text-zinc-400/90 text-xs'
-    >
+    return (
+        <footer
+            className='w-full bg-zinc-100  flex gap-5 justify-center flex-col items-center  py-4 px-4 text-zinc-400/90 text-xs'
+        >
 
-        <div className='flex  justify-around gap-5 w-full'>
-        <div className='flex flex-col gap-2 justify-center items-start'>
-            <h3 className=' text-lg  text-black '>About</h3>
-            <p>
-                About azadBazar group
-            </p>
-            <p>careers</p>
-            <p>
-                Contact us
-            </p>
-        </div>
-        <div className='flex flex-col gap-2 justify-center items-start'>
-            <h3 className=' text-lg text-black '>AzadBazar</h3>
-            <p>
-                Help
-            </p>
-            <p>
-            Privacy Policy
-        </p>
-        <p>
-            Terms of Service
-        </p>
+            <div className='flex  justify-around gap-5 w-full'>
+                <div className='flex flex-col gap-2 justify-center items-start'>
+                    <h3 className=' text-lg  text-black '>About</h3>
+                    <p>
+                        About azadBazar 
+                    </p>
+                    <p>careers</p>
+                    <p>
+                        Contact us
+                    </p>
+                </div>
+                <div className='flex flex-col gap-2 justify-center items-start'>
+                    <h3 className=' text-lg text-black '>AzadBazar</h3>
+                    <p>
+                        Help
+                    </p>
+                    <p>
+                        Privacy Policy
+                    </p>
+                    <p>
+                        Terms of Service
+                    </p>
 
-        </div>
-        <div className='flex flex-col gap-2 justify-center items-start'>
-            <h3 className=' text-lg text-black '>Menu</h3>
-            <p>
-                Home
-            </p>
-            <p>
-                Categories
-            </p>
-            <p>
-                Posts
-            </p>
+                </div>
+                <div className='flex flex-col gap-2 justify-center items-start'>
+                    <h3 className=' text-lg text-black '>Menu</h3>
+                    <p>
+                        Home
+                    </p>
+                    <p>
+                        Categories
+                    </p>
+                    <p>
+                        Posts
+                    </p>
 
-        </div>
-        
-           
-        
-        </div>
-        <div className='flex  gap-2  justify-center items-center'>
-            <h3 className='  text-balance text-black  '>falow us </h3>
-            <p>
-                Facebook
-            </p>
-            <p>
-                Twitter
-            </p>
-            <p>
-                Instagram
-            </p>
-        </div>
-        <div>
-        <p>
-            © 2024 AzadBazar. All rights reserved.
-        </p>
-        </div>
-       
-    </footer>
-  )
+                </div>
+
+
+
+            </div>
+            <div className='flex  gap-2  justify-center items-center'>
+                <h3 className='  text-[15px] text-black  '>falow us at</h3>
+                <p>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" color="#000000" fill="none">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M6.18182 10.3333C5.20406 10.3333 5 10.5252 5 11.4444V13.1111C5 14.0304 5.20406 14.2222 6.18182 14.2222H8.54545V20.8889C8.54545 21.8081 8.74951 22 9.72727 22H12.0909C13.0687 22 13.2727 21.8081 13.2727 20.8889V14.2222H15.9267C16.6683 14.2222 16.8594 14.0867 17.0631 13.4164L17.5696 11.7497C17.9185 10.6014 17.7035 10.3333 16.4332 10.3333H13.2727V7.55556C13.2727 6.94191 13.8018 6.44444 14.4545 6.44444H17.8182C18.7959 6.44444 19 6.25259 19 5.33333V3.11111C19 2.19185 18.7959 2 17.8182 2H14.4545C11.191 2 8.54545 4.48731 8.54545 7.55556V10.3333H6.18182Z" stroke="currentColor" stroke-width="1.5" stroke-linejoin="round" />
+                    </svg>
+                </p>
+                <p>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20"  color="#000000" fill="none">
+                        <path d="M2 18.5C3.76504 19.521 5.81428 20 8 20C14.4808 20 19.7617 14.8625 19.9922 8.43797L22 4.5L18.6458 5C17.9407 4.37764 17.0144 4 16 4C13.4276 4 11.5007 6.51734 12.1209 8.98003C8.56784 9.20927 5.34867 7.0213 3.48693 4.10523C2.25147 8.30185 3.39629 13.3561 6.5 16.4705C6.5 17.647 3.5 18.3488 2 18.5Z" stroke="currentColor" stroke-width="1.5" stroke-linejoin="round" />
+                    </svg>
+                </p>
+                <p>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"width="20" height="20" color="#000000" fill="none">
+                        <path d="M2.5 12C2.5 7.52166 2.5 5.28249 3.89124 3.89124C5.28249 2.5 7.52166 2.5 12 2.5C16.4783 2.5 18.7175 2.5 20.1088 3.89124C21.5 5.28249 21.5 7.52166 21.5 12C21.5 16.4783 21.5 18.7175 20.1088 20.1088C18.7175 21.5 16.4783 21.5 12 21.5C7.52166 21.5 5.28249 21.5 3.89124 20.1088C2.5 18.7175 2.5 16.4783 2.5 12Z" stroke="currentColor" stroke-width="1.5" stroke-linejoin="round" />
+                        <path d="M16.5 12C16.5 14.4853 14.4853 16.5 12 16.5C9.51472 16.5 7.5 14.4853 7.5 12C7.5 9.51472 9.51472 7.5 12 7.5C14.4853 7.5 16.5 9.51472 16.5 12Z" stroke="currentColor" stroke-width="1.5" />
+                        <path d="M17.5078 6.5L17.4988 6.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </p>
+                <p>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20"  color="#000000" fill="none">
+                        <path d="M2.5 12C2.5 7.52166 2.5 5.28249 3.89124 3.89124C5.28249 2.5 7.52166 2.5 12 2.5C16.4783 2.5 18.7175 2.5 20.1088 3.89124C21.5 5.28249 21.5 7.52166 21.5 12C21.5 16.4783 21.5 18.7175 20.1088 20.1088C18.7175 21.5 16.4783 21.5 12 21.5C7.52166 21.5 5.28249 21.5 3.89124 20.1088C2.5 18.7175 2.5 16.4783 2.5 12Z" stroke="currentColor" stroke-width="1.5" stroke-linejoin="round" />
+                        <path d="M10.5359 11.0075C9.71585 10.8916 7.84666 11.0834 6.93011 12.7782C6.01355 14.4729 6.9373 16.2368 7.51374 16.9069C8.08298 17.5338 9.89226 18.721 11.8114 17.5619C12.2871 17.2746 12.8797 17.0603 13.552 14.8153L13.4738 5.98145C13.3441 6.95419 14.4186 9.23575 17.478 9.5057" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </p>
+            </div>
+            <div>
+                <p>
+                    © 2024 AzadBazar. All rights reserved.
+                </p>
+            </div>
+
+        </footer>
+    )
 }
 
 export default Footer
