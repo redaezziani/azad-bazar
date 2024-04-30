6f1f49c164c88df34e923a0a260a93b1cd170f15
rachidfrex <2003043000250@ofppt-edu.ma>
2024-04-30 18:20:40 +0100

    Update styling in footer.tsx

--- src/components/home-page/footer.tsx
+++ src/components/home-page/footer.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer
-    className='w-full bg-zinc-100  flex justify-center flex-col items-center gap-4 py-4 px-4 text-zinc-400/90 text-xs'
+    className='w-full bg-zinc-100  flex gap-5 justify-center flex-col items-center  py-4 px-4 text-zinc-400/90 text-xs'
     >
 
-        <div className='flex  justify-between gap-5 w-full'>
+        <div className='flex  justify-around gap-5 w-full'>
         <div className='flex flex-col gap-2 justify-center items-start'>
             <h3 className=' text-lg  text-black '>About</h3>
             <p>
@@ -18,7 +18,7 @@ const Footer = () => {
             </p>
         </div>
         <div className='flex flex-col gap-2 justify-center items-start'>
-            <h3 className=' text-lg text-black '>AZADBAZAR</h3>
+            <h3 className=' text-lg text-black '>AzadBazar</h3>
             <p>
                 Help
             </p>
@@ -31,17 +31,33 @@ const Footer = () => {
 
         </div>
         <div className='flex flex-col gap-2 justify-center items-start'>
-            <h3 className=' text-lg  '>About</h3>
+            <h3 className=' text-lg text-black '>Menu</h3>
             <p>
-                About azadBazar group
+                Home
             </p>
-            <p>careers</p>
             <p>
-                Contact us
+                Categories
             </p>
+            <p>
+                Posts
+            </p>
+
         </div>
+        
            
         
+        </div>
+        <div className='flex  gap-2  justify-center items-center'>
+            <h3 className='  text-balance text-black  '>falow us </h3>
+            <p>
+                Facebook
+            </p>
+            <p>
+                Twitter
+            </p>
+            <p>
+                Instagram
+            </p>
         </div>
         <div>
         <p>
